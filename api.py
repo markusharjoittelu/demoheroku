952bf04b9c4bc38ee4e2011dae48a31a8ea9f046
Markus <niiranen.markus@gmail.com>
2021-02-16 13:17:17 +0200

    python api file for heroku

---
+++ api.py
@@ -0,0 +1,37 @@
+from flask import Flask
+from flask import jsonify
+from sqlalchemy import create_engine, text
+import os
+from dotenv import load_dotenv
+
+app = Flask(__name__)
+load_dotenv()
+
+
+def get_db_data():
+    DATABASE_URL = os.environ['DATABASE_URL']
+    db = create_engine(DATABASE_URL)
+
+    sql = text('SELECT * FROM datas')
+    results = db.execute(sql)
+    return results
+
+
+@app.route('/api')
+def api():
+    results = get_db_data()
+    print('hello')
+    print(type(results))
+    data = []
+    line = 0 
+
+    for row in results:
+        r = [row['times'],row['temperature'], row['humidity']]
+        data.insert(line, r)
+        line +=1
+
+    return jsonify({'data': data})
+
+if __name__ == '__main__':
+    app.config['DEBUG']=True
+    app.run(threaded=True, port=5000)
