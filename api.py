2f92e8ff4f25c9ddf3a72666d2fb65411be16b36
Markus <niiranen.markus@gmail.com>
2021-02-16 22:26:56 +0200

    added cors support

--- api.py
+++ api.py
@@ -3,8 +3,10 @@ from flask import jsonify
 from sqlalchemy import create_engine, text
 import os
 from dotenv import load_dotenv
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 load_dotenv()
 
 
