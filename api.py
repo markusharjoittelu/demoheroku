02137337c8c5dc793644dd4b00d749a654cc8f4e
Markus <niiranen.markus@gmail.com>
2021-02-17 22:44:52 +0200

    added comments

--- api.py
+++ api.py
@@ -6,10 +6,12 @@ from dotenv import load_dotenv
 from flask_cors import CORS
 
 app = Flask(__name__)
+# Allow Cross Origin Resource Sharing for all origins
+# For demo use only
 CORS(app)
 load_dotenv()
 
-
+# Create SQL query using sqlalchemy params to get data from DB
 def get_db_data():
     DATABASE_URL = os.environ['DATABASE_URL']
     db = create_engine(DATABASE_URL)
@@ -17,16 +19,19 @@ def get_db_data():
     results = db.execute(sql)
     return results
 
+# Data server root route, nothing here
 @app.route('/')
 def index():
     return ('Welcome to get temperature and humidity data')
 
+# Data server api route for getting data in json form
 @app.route('/api')
 def api():
     results = get_db_data()
     data = []
     line = 0 
 
+    # Inset data from DB to list for jsonify function
     for row in results:
         r = [row['id'],row['times'],row['temperature'], row['humidity']]
         data.insert(line, r)
